04b0578286427b522529888ca8aec1942b5beb28
planoraforstudents <planoraforstudents@gmail.com>
2025-12-28 02:20:22 +0530

    fix: update STATIC_URL to /django_static/ to resolve Nginx conflicts

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -155,7 +155,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/django_static/'
 STATIC_ROOT = BASE_DIR / 'staticfiles'
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 MEDIA_URL = '/media/'
