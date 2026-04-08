8d75b4666951736f3996b33555afb1b38e46d0c5
planoraforstudents <planoraforstudents@gmail.com>
2026-04-09 01:46:15 +0530

    chore: commit all changes before push

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -7,6 +7,12 @@ from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 from django.conf import settings
 from django.conf.urls.static import static
 from dashboard import views as dashboard_views
+from rest_framework_simplejwt.views import TokenObtainPairView as DefaultTokenObtainPairView
+from users.serializers import CustomTokenObtainPairSerializer
+
+
+class CustomTokenObtainPairView(DefaultTokenObtainPairView):
+    serializer_class = CustomTokenObtainPairSerializer
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -48,7 +54,7 @@ urlpatterns = [
     # Backward-compatible alias used by current production clients
     path('planora/', include(('planora.urls', 'planora'), namespace='planora_legacy')),
     # JWT token endpoints
-    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('api/token/', CustomTokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 ]
 
